b79ade40c0f490397e7a6d0be268968c35a42d9b
Ellen Marie Dash <me@duckie.co>
2024-10-21 16:50:05 -0400

    made a lot of progress on the general ui

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,8 @@ fn main() {
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 enum View {
-    Fetching,
-    Config,
     Home,
+    ArtistList,
     Album(Album),
     Artist(String),
 }
@@ -51,6 +50,9 @@ struct ShorkApp {
 
     artists: HashMap<String, Vec<Album>>,
 
+    show_config: bool,
+    fetching_data: bool,
+
     view: View,
 }
 
@@ -58,13 +60,15 @@ impl ShorkApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         let (tx, rx) = std::sync::mpsc::channel();
 
-        let artists = HashMap::new();
-
-        let view = View::Home;
-
-        let config = Config::default();
-
-        let mut slf = Self { tx, rx, config, artists, view };
+        let mut slf = Self {
+            tx: tx,
+            rx: rx,
+            config: Config::default(),
+            artists: HashMap::new(),
+            show_config: false,
+            fetching_data: false,
+            view: View::Home,
+        };
 
         if let Some(storage) = cc.storage {
             if let Some(config) = eframe::get_value(storage, "shork/config") {
@@ -83,20 +87,6 @@ impl ShorkApp {
         slf
     }
 
-    fn update_config(&mut self, ctx: &egui::Context) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Configuration");
-            ui.label("Server");
-            ui.text_edit_singleline(&mut self.config.server);
-            ui.label("API Token");
-            ui.text_edit_singleline(&mut self.config.token);
-            if ui.button("Done").clicked() {
-                self.fetch_data(ctx);
-                self.view = View::Fetching;
-            }
-        });
-    }
-
     fn fetch_data(&self, ctx: &egui::Context) {
         fetch_info(self.config.clone(), self.tx.clone(), ctx.clone());
     }
@@ -112,103 +102,121 @@ impl eframe::App for ShorkApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         if let Ok(artists) = self.rx.try_recv() {
             self.artists = artists;
-            if self.view == View::Fetching {
-                self.view = View::Home;
-            }
+            self.fetching_data = false;
         }
 
         if self.config.server.is_empty() || self.config.token.is_empty() {
-            self.view = View::Config;
+            self.show_config = true;
         }
-
-        if self.view == View::Config {
-            self.update_config(ctx);
-            return;
-        }
-
-        egui::TopBottomPanel::top("top-panel").show(ctx, |ui| {
-            ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
-                if ui.button("Configure").clicked() {
-                    self.view = View::Config;
-                }
-
-                if ui.button("Refresh").clicked() {
-                    fetch_info(self.config.clone(), self.tx.clone(), ctx.clone());
-                }
-
-                if ui.button("Artists").clicked() {
-                    self.view = View::Home;
-                }
-
-                if let View::Artist(artist_name) = &self.view {
-                    if ui.button(artist_name).clicked() {
-                        self.view = View::Artist(artist_name.clone());
-                    }
-                }
-
-                if let View::Album(album) = &self.view {
-                    let name = album.name.clone();
-                    if ui.button(&album.artist_name).clicked() {
-                        self.view = View::Artist(album.artist_name.clone());
-                    }
-
-                    let _ = ui.button(&name);
-                }
-            });
-        });
 
         egui::TopBottomPanel::bottom("bottom-panel").show(ctx, |ui| {
             ui.label("bottom");
+
+            if ui.button("Configure").clicked() {
+                self.show_config = true;
+            }
+        });
+
+        egui::SidePanel::left("left-panel").show(ctx, |ui| {
+            ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {
+                if ui.button("Home").clicked() {
+                    self.view = View::Home;
+                }
+
+                if ui.button("Artists").clicked() {
+                    self.view = View::ArtistList;
+                }
+            });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP).with_main_wrap(true), |ui| {
-                    match self.view.clone() {
-                        View::Album(album) => self.view_album(ctx, ui, &album),
-                        View::Artist(artist_name) => self.view_artist(ctx, ui, &artist_name),
-                        View::Home => self.view_home(ctx, ui),
-                        View::Fetching => self.view_fetching(ctx, ui),
-                        View::Config => { /* do nothing */ },
-                    }
-                });
-            });
+            if self.fetching_data {
+                self.view_fetching(ctx, ui);
+                return;
+            }
+
+            if self.show_config {
+                self.view_config(ctx, ui);
+                return;
+            }
+
+            match self.view.clone() {
+                View::Album(album) => self.view_album(ctx, ui, &album),
+                View::Artist(artist_name) => self.view_artist(ctx, ui, &artist_name),
+                View::ArtistList => self.view_artist_list(ctx, ui),
+                View::Home => self.view_home(ctx, ui),
+            }
         });
     }
 }
 
 impl ShorkApp {
+    fn view_config(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
+        ui.heading("Configuration");
+        ui.label("Server");
+        ui.text_edit_singleline(&mut self.config.server);
+        ui.label("API Token");
+        ui.text_edit_singleline(&mut self.config.token);
+        if ui.button("Refresh Data").clicked() {
+            self.fetch_data(ctx);
+            self.show_config = false;
+            self.fetching_data = true;
+        }
+    }
+
     fn view_fetching(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
         ui.heading("Fetching data from server.");
     }
 
     fn view_home(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
-        for (artist_name, _albums) in &self.artists {
-            let btn = egui::Button::opt_image_and_text(None, Some(artist_name.into()))
-                .wrap_mode(egui::TextWrapMode::Truncate);
+        ui.heading("TODO");
+        self.view = View::ArtistList;
+    }
 
-            if ui.add_sized(egui::vec2(100.0, 100.0), btn).clicked() {
-                self.view = View::Artist(artist_name.clone());
-            }
-        }
+    fn view_artist_list(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP).with_main_wrap(true), |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for (artist_name, _albums) in &self.artists {
+                    let btn = egui::Button::opt_image_and_text(None, Some(artist_name.into()))
+                        .wrap_mode(egui::TextWrapMode::Truncate);
+
+                    if ui.add_sized(egui::vec2(100.0, 100.0), btn).clicked() {
+                        self.view = View::Artist(artist_name.clone());
+                    }
+                }
+            });
+        });
     }
 
     fn view_artist(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui, artist_name: &str) {
-        for album in &self.artists[artist_name] {
-            let btn = egui::Button::opt_image_and_text(None, Some(album.name.clone().into()))
-                .wrap_mode(egui::TextWrapMode::Truncate);
-            if ui.add_sized(egui::vec2(100.0, 100.0), btn).clicked() {
-                self.view = View::Album(album.clone());
-            }
-        }
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP).with_main_wrap(true), |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for album in &self.artists[artist_name] {
+                    let btn = egui::Button::opt_image_and_text(None, Some(album.name.clone().into()))
+                        .wrap_mode(egui::TextWrapMode::Truncate);
+                    if ui.add_sized(egui::vec2(100.0, 100.0), btn).clicked() {
+                        self.view = View::Album(album.clone());
+                    }
+                }
+            });
+        });
     }
 
     fn view_album(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui, album: &Album) {
-        for track in &album.tracks {
-            if ui.button(&track.name).clicked() {
-                println!("{:?}", track.stream_url);
+        egui::ScrollArea::vertical().show(ui, |ui| {
+            ui.heading(&album.name);
+            if ui.button(&album.artist_name).clicked() {
+                self.view = View::Artist(album.artist_name.clone());
             }
-        }
+
+            ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {
+                for track in &album.tracks {
+                    if ui.button(&track.name).clicked() {
+                        println!("{:?}", track.stream_url);
+                    }
+                }
+            });
+        });
     }
 }
 
